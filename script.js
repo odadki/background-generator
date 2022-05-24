11c7c278a6e3f189bb389f1a951cbece77ada1cd
odadki <93558662+odadki@users.noreply.github.com>
2022-05-24 12:46:33 -0700

    added all css, html, js files

---
+++ script.js
@@ -0,0 +1,15 @@
+let h3 = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("body-gradient");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
